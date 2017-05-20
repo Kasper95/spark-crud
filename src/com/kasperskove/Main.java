2255a65cc12a0cf5f34a8ff33f64ec094e6a6126
kasper95 <kove_kasper@outlook.com>
2017-05-20 12:21:38 -0400

    +r/templates/home.html, +get route to test

--- src/com/kasperskove/Main.java
+++ src/com/kasperskove/Main.java
@@ -1,8 +1,25 @@
 package com.kasperskove;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Spark.init();
+
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    HashMap m = new HashMap();
+                    m.put("name", "Kaela");
+                    return new ModelAndView(m, "home.html");
+                }),
+                new MustacheTemplateEngine()
+        );
     }
 }
